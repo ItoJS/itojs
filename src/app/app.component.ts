83b9e920cf6a449cd391e11ea50849d7c3c9fae4
ItoJS <contact_nic@aguisacorp.com>
2020-05-01 17:56:11 -0600

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Beats CR - Registration follow-up';
+  name = 'Angular';
 }
