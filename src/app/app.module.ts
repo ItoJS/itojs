739ca29015814e5af3ee78b0c9f8513880a2a3be
ItoJS <contact_nic@aguisacorp.com>
2020-05-01 15:48:31 -0600

    2 files modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { LoginModuleComponent } from './login-module/login-module.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, LoginModuleComponent, ProductAlertsComponent ],
+  declarations: [ AppComponent, HelloComponent, LoginModuleComponent, ProductAlertsComponent, ProductListComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
